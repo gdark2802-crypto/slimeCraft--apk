ea70ab99f9eba64d2a15ec7c2b33b4b157654f1c
gdark2802-crypto <gdark2802@gmail.com>
2026-09-20 17:55:06 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,2 @@
 import 'package:flutter/material.dart';
-void main() => runApp(const MaterialApp(home: Scaffold(body: Center(child: Text('SlimeCraft OK', style: TextStyle(fontSize: 30))))));
+void main()=>runApp(MaterialApp(home:Text('OK')));
